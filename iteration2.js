e927f0bc7b6bcc4a292a145e55b764304fd26226
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-10-05 14:59:59 +0200

    iteration2

---
+++ iteration2.js
@@ -0,0 +1,12 @@
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(avengers) {
+    let larger=avengers[0];
+    for (let index = 1; index < avengers.length; index++) {
+        if (avengers[index].length>larger.length) {
+            larger=avengers[index];
+        }
+        
+    }
+    return larger;
+}
+console.log(findLongestWord(avengers));
