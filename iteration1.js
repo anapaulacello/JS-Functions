12526993ccadcbfdfb744f6f73484d2a2a4017b9
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-10-05 14:45:09 +0200

    iteration1

---
+++ iteration1.js
@@ -0,0 +1,12 @@
+function sum(numberOne , numberTwo) {
+    let larger;
+    if (numberOne>numberTwo) {
+        larger=numberOne;
+    }
+    else{
+        larger=numberOne;
+    }
+    return larger;
+}
+console.log(sum(4,3));
+
