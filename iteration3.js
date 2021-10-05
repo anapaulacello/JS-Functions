4534d5db0b83c9e077293c7c80310f7aaa2f95a9
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-10-05 15:30:10 +0200

    iteration4

--- iteration3.js
+++ iteration3.js
@@ -2,7 +2,7 @@ const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumNumbers(numeros) {
     let sum=0;
-    for (let index = 0; index < numbers.length; index++) {
+    for (let index = 0; index < numeros.length; index++) {
         sum+=numbers[index];
         
     }
---
+++ iteration4.js
@@ -0,0 +1,10 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+    let sum=0;
+    for (let index = 0; index < numeros.length; index++) {
+        sum+=numbers[index];
+    }
+    let promedio=sum/numbers.length;
+
+}
+console.log(average(numbers));
